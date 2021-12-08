f0f9f32cdd23151f8a08d8bdcad2c49d116a6c65
Ruta Reisoglu <rutareiso@protonmail.com>
2021-12-08 00:16:21 +0000

    set up database files

---
+++ db/urlDatabase.js
@@ -0,0 +1,8 @@
+const urlDatabase = {
+  "b2xVn2":{longURL: "http://www.lighthouselabs.ca", dateCreated: "2021/12/04"},
+  "9sm5xK":{longURL: "http://www.google.com", dateCreated: "2021/12/05"},
+};
+
+module.exports = {
+  urlDatabase
+};
